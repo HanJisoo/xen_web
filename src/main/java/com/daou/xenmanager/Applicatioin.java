ecba9a68e58f5a585a08eab54b670c9d2036567e
한지수 <thecarlos@daou.co.kr>
2016-12-07 15:19:40 +0900

    initial commit

---
+++ src/main/java/com/daou/xenmanager/Applicatioin.java
@@ -0,0 +1,14 @@
+package com.daou.xenmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by user on 2016-12-07.
+ */
+@SpringBootApplication
+public class Applicatioin {
+        public static void main(String[] args){
+            SpringApplication.run(Applicatioin.class, args);
+        }
+}
---
+++ src/main/java/com/daou/xenmanager/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.daou.xenmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by user on 2016-12-07.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index(){
+        return init();
+    }
+
+    public String init(){
+        return "kk";
+    }
+
+}
