5e014b3fcc9246f71645238744b32ba578492884
한지수 <thecarlos@daou.co.kr>
2016-12-13 15:15:43 +0900

    import library

---
+++ src/main/java/com/daou/xenmanager/XenManagerApplication.java
@@ -0,0 +1,21 @@
+package com.daou.xenmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by user on 2016-12-07.
+ */
+@SpringBootApplication
+public class XenManagerApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XenManagerApplication.class);
+    }
+
+    public static void main(String[] args){
+            SpringApplication.run(XenManagerApplication.class, args);
+        }
+}
--- src/main/java/com/daou/xenmanager/controller/MainController.java
+++ src/main/java/com/daou/xenmanager/controller/MainController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by user on 2016-12-07.
  */
@@ -18,23 +21,47 @@ public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String index(){
-        return init();
+        return test();
     }
 
     public String init(){
         String result = "fail";
-
+        String kkk = "";
         try{
             handle = new STAFHandle("Xen-Manager");
         }catch (STAFException e){
             return "Error registering wigth STAF, RC : " + e.rc;
         }
-
-        STAFResult sr = handle.submit2("local", "test", "list");
-
+        //STAFResult sr = handle.submit2("local", "xen_manager", "list snap-shot");
+        STAFResult sr = handle.submit2("local", "xen_manager", "list snap-shot");
         if(sr.rc != 0){
-            result = "RC : " + sr.rc;
+            result = "RC : " + sr.rc + ", " + sr.result;
         }else{
+            result = sr.result.substring(1, sr.result.length()-1);
+            String[] keyValuePairs = result.split(",");
+            Map<String, String> map = new HashMap<>();
+            for(String pair : keyValuePairs){
+                String[] item = pair.split("=");
+                map.put(item[0], item[1]);
+            }
+            result = map.toString();
+        }
+        return result;
+    }
+
+    public String test(){
+        String result = "fail";
+        try{
+            handle = new STAFHandle("Xen-Manager");
+        }catch (STAFException e){
+            return "Error registering wigth STAF, RC : " + e.rc;
+        }
+        //STAFResult sr = handle.submit2("local", "xen_manager", "list snap-shot");
+        STAFResult sr = handle.submit2("local", "xen_manager", "list snap-shot");
+        if(sr.rc != 0){
+            result = "RC : " + sr.rc + ", " + sr.result;
+        }else{
+            //result = sr.resultObj.toString();
             result = sr.result;
         }
 
